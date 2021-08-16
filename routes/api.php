ea1ac554559470eecf2a1c8f8999904ea3429ae4
Vo Truong <votruong@MacBook-Pro-cua-Vo.local>
2021-08-16 19:58:05 +0700

    add source

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/change-pass', [AuthController::class, 'changePassword']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+    Route::post('/change-user-profile', [AuthController::class, 'changeProfile']);    
+});
